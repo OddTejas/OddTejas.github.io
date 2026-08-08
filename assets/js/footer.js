30b9388195acf32d731314be19f76eb4cb52752b
OddTejas <oddtejas.lab@gmail.com>
2026-08-08 15:12:17 +0530

    footer injector js script with logotext now acts as a link

---
+++ assets/js/footer.js
@@ -0,0 +1,17 @@
+const footer= document.querySelector('#footer');
+footer.innerHTML=
+`           <section class="footerBrand">
+           <p class="footerTitle">OddHouse</p>
+                <p>Made with intention</p>
+            </section>
+
+            <section class="footerGroup">
+                <p class="footerLabel">Contact</p>
+                <a href="mailto:oddtejas.labs@gmail.com">oddtejas.labs@gmail.com</a>
+            </section>
+
+            <section class="footerGroup">
+                <p class="footerLabel">Photography</p>
+                <p>© Evan Tejas. All photographs are protected by copyright.</p>
+                <p>Please contact me for licensing or permitted use.</p>
+            </section>`;
